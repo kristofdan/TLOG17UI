4c14a42898ecbfa898b90b0629558c243fc0972e
Kristóf Dan <dankristof95@gmail.com>
2017-08-21 11:09:34 +0200

    Body of the calendar table is now dynamically created with JS

--- TLOG16UI/public_html/scripts/calendar.js
+++ TLOG16UI/public_html/scripts/calendar.js
@@ -18,14 +18,6 @@ $(document).ready(function(){
     });
 });
 
-
-
-function drawAndFillTableBody(date){
-    var tbody = createAndFillTbody(date);
-        
-    $("#calendar-table").append(tbody);
-}
-
 function setCalendarCaption(date){
     
     var monthNames = ["January", "February", "March", "April", "May", "June",
@@ -33,6 +25,12 @@ function setCalendarCaption(date){
     ];
     $("#calendar-table caption").text(date.getFullYear() + " "
             + monthNames[date.getMonth()]);
+}
+
+function drawAndFillTableBody(date){
+    var tbody = createAndFillTbody(date);
+        
+    $("#calendar-table").append(tbody);
 }
 
 function createAndFillTbody(date){
@@ -60,8 +58,8 @@ function calculateTbodySize(date){
     var cellNumber = dayNumber + firstRowEmptyCellNumber;
     var rowNumber = cellNumber / 7;
     rowNumber = Math.ceil(rowNumber);
-    var lastRowEmptyCellNumber = (firstRowEmptyCellNumber + dayNumber) -
-            (rowNumber * 7);
+    var lastRowEmptyCellNumber = (rowNumber * 7) -
+            (firstRowEmptyCellNumber + dayNumber);
     
     var tbodySize = {rowNumber: rowNumber,
         firstRowEmptyCellNumber: firstRowEmptyCellNumber,
@@ -74,36 +72,80 @@ function calculateTbodySize(date){
 function createAndFillCells(tbodySize){
     var tbody = document.createElement("tbody");
     
-    var firstRow = createAndFillFirstRow(tbodySize);
+    var firstRow = createAndFillFirstRow(tbodySize.firstRowEmptyCellNumber);
     tbody.appendChild(firstRow);
     
     var currentDay = 7 - tbodySize.firstRowEmptyCellNumber + 1;
-    for (var i=1; i<tbodySize.rowNumber - 1; ++i){
-        var newRow = document.createElement("tr");
+    tbody = createMiddleRowsInTbody(tbody, tbodySize.rowNumber, currentDay);
+     
+    var firstDayOfLastRow = currentDay + (tbodySize.rowNumber - 2) * 7;
+    var lastRow = createAndFillLastRow(firstDayOfLastRow, tbodySize.lastRowEmptyCellNumber);
+    tbody.appendChild(lastRow);
+    
+    return tbody;
+}
+
+function createAndFillFirstRow(firstRowEmptyCellNumber){
+    var currentDay = 1;
+    var firstRow = document.createElement("tr");
+    for (var i=0; i<firstRowEmptyCellNumber; ++i){
         var newCell = document.createElement("td");
-        var textNode = document.createTextNode("This is a new row.");
-        newCell.appendChild(textNode);
-        newRow.appendChild(newCell);
-        tbody.appendChild(newRow);
+        firstRow.appendChild(newCell);
+    }
+    for (var i=firstRowEmptyCellNumber; i<7; ++i){
+        var newCell = createDayCell(currentDay);
+        firstRow.appendChild(newCell);
+        currentDay++;
+    }
+    return firstRow;
+}
+
+function createMiddleRowsInTbody(tbody, rowNumber, currentDay){
+    for (var i=1; i<rowNumber - 1; ++i){
+        var row = createFullRow(currentDay + (i-1)*7);
+        tbody.appendChild(row);
     }
     return tbody;
 }
 
-function createAndFillFirstRow(tbodySize){
-    var currentDay = 1;
-    var firstRow = document.createElement("tr");
-    for (var i=0; i<tbodySize.firstRowEmptyCellNumber; ++i){
-        var newCell = document.createElement("td");
-        firstRow.appendChild(newCell);
-    }
-    for (var i=tbodySize.firstRowEmptyCellNumber; i<7; ++i){
-        var newCell = document.createElement("td");
-        var textNode = document.createTextNode(currentDay);
+function createFullRow(currentDay){
+    var row = document.createElement("tr");
+    for (var i = 0; i < 7; i++) {
+        var newCell = createDayCell(currentDay);
+        row.appendChild(newCell);
         currentDay++;
-        newCell.appendChild(textNode);
-        firstRow.appendChild(newCell);
     }
-    return firstRow;
+    return row;
+}
+
+function createAndFillLastRow(firstDayOfLastRow, lastRowEmptyCellNumber){
+    var lastRow = document.createElement("tr");
+    var lastDay = firstDayOfLastRow + (7 - lastRowEmptyCellNumber - 1);
+    for (var i=firstDayOfLastRow; i<=lastDay; i++){
+        var newCell = createDayCell(i);
+        lastRow.appendChild(newCell);
+    }
+    for (var i=lastDay + 1; i<=lastDay + lastRowEmptyCellNumber; i++){
+        var newCell = document.createElement("td");
+        lastRow.appendChild(newCell);
+    }
+    
+    return lastRow;
+}
+
+function createDayCell(currentDay){
+    var newCell = document.createElement("td");
+    var dayNumber = document.createTextNode(currentDay);
+    var lineBreak = document.createElement("br");
+    var extraMinutes = document.createElement("span");
+    extraMinutes.style.fontSize = "0.8em";
+    var extraMinutesText = document.createTextNode("100");
+
+    extraMinutes.appendChild(extraMinutesText);
+    newCell.appendChild(dayNumber);
+    newCell.appendChild(lineBreak);
+    newCell.appendChild(extraMinutes);
+    return newCell;
 }
 
 function calculateDateOfPreviousMonth(){
